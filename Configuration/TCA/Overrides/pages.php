3b7d59739c8383bdad0f0314c582b3c6bf6b19d6
Yannick Aister <yannick.aister@aistee.de>
2023-02-08 17:03:06 +0100

    init

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile('aisteaportfolio','Configuration/TSconfig/BackendLayouts.tsconfig','Default Backendlayouts');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile('aisteaportfolio','Configuration/TSconfig/Page_general.tsconfig','Default PageTSconfig');
--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('aisteaportfolio', 'Configuration/TypoScript/', 'TYPO3 11 Extension Template');
